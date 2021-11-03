df1030fd12c8ff5ce26ac5b4eed0409153a50446
emmanoel1965@gmail.com <emmanoel1965@gmail.com>
2021-11-03 16:38:57 -0300

    Creating navigation Stack

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Home } from '../screens/Home';
+import { SignIn } from '../screens/SignIn';
+
+const Stack = createNativeStackNavigator();
+
+export function AuthRoutes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+};
